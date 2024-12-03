2f996475a16d216df2df18a2d466cfb236ea7c2e
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-12-03 15:57:25 +0000

    updated sidebar function

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react';
+import css from "./css/Sidebar.module.css";
 
-const Sidebar = () => {
+function Sidebar() {
     return (
-        <div>
-            <a href="#" target="_blank">My Photos</a>
-            <a href="#" target="_blank">My Illustrations</a>
-            <a href="#" target="_blank">My Paintings</a>
+        <div className={css.sidebar}>
+            <a href = "#." target= "_blank">My Photos</a>
+            <a href = "#." target= "_blank">My Illustrations</a>
+            <a href = "#." target= "_blank">My Paintings</a>
         </div>
-    );
+    )
 }
 
-export default Sidebar;
+export default Sidebar
