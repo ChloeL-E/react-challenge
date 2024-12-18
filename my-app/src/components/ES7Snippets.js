6039a2c7800050af6fd043231413d0baa5e51f3e
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-12-18 21:25:56 +0000

    added ES7 cheatsheet for info

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,55 @@
+//import React from 'react'
+
+// function ES7Snippets() {
+//  return (
+//    <div>ES7Snippets</div>
+//  )
+//}
+
+//export default ES7Snippets    
+
+// to get the above code snippet automatically just type rfce + tab to create a React Functional component
+
+
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// above snippet is rafce then tab - creates and exports a React component as an arrow function
+
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets 
+
+// rce then tab. this snippet creates and exports a class component where it imports {component} directly and extends it
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      
+//   }
+// }
+
+// rcons and tab for creating a constructor that contains props as arguments
+
+console.log(object)
+//clg to log something to console
+console.log('object', object)
+//clo to log name and object to console
